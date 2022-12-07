671648fba9e36c82823a197a6b7d05ead322cf3d
Nick <ko.binghong@gmail.com>
2022-12-07 08:09:24 -0500

    2022/12/07/08:09 by Nick

--- MiniProjectPath1.py
+++ MiniProjectPath1.py
@@ -1,6 +1,6 @@
 import pandas
 
-''' 
+'''
 The following is the starting code for path1 for data reading to make your first step easier.
 'dataset_1' is the clean data for path1.
 '''
@@ -9,5 +9,5 @@ dataset_1['Brooklyn Bridge']      = pandas.to_numeric(dataset_1['Brooklyn Bridge
 dataset_1['Manhattan Bridge']     = pandas.to_numeric(dataset_1['Manhattan Bridge'].replace(',','', regex=True))
 dataset_1['Queensboro Bridge']    = pandas.to_numeric(dataset_1['Queensboro Bridge'].replace(',','', regex=True))
 dataset_1['Williamsburg Bridge']  = pandas.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
-dataset_1['Williamsburg Bridge']  = pandas.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
+# dataset_1['Williamsburg Bridge']  = pandas.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
 # print(dataset_1.to_string()) #This line will print out your data
