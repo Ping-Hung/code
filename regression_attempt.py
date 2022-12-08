b9302ec3241e70492a2a487cb2e2e0c7cbd73a44
Nick <ko.binghong@gmail.com>
2022-12-08 12:39:19 -0500

    2022/12/08/12:39 Nick -just parsing data for later regression use

--- regression_attempt.py
+++ regression_attempt.py
@@ -1,9 +1,16 @@
-form from MiniProjectPath1 import dataset_1
+from MiniProjectPath1 import dataset_1
+import numpy as np
 import datetime
-import sklearn import linear
+from sklearn.linear_model import LinearRegression
+
+def input_X_matrix():
+    """returns a 5 column matrix such that data for each bridge is recorded in a single colmn vector"""
+    Brooklyn, Manhattan = list(dataset_1['Brooklyn Bridge']), list(dataset_1['Manhattan Bridge'])
+    Williamsburg, Queensboro = list(dataset_1['Williamsburg Bridge']), list(dataset_1['Queensboro Bridge'])
+    return np.array([Brooklyn, Manhattan, Williamsburg, Queensboro], 'F')
+
 
 def main():
-
 
 if __name__ == '__main__':
     main()
