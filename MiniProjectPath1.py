aa6a067d81b87698151e8dd56727f4ac79683db9
Nick <ko.binghong@gmail.com>
2022-12-08 16:45:38 -0500

    2022/12/08/16:45--Nick

--- MiniProjectPath1.py
+++ MiniProjectPath1.py
@@ -9,5 +9,5 @@ dataset_1['Brooklyn Bridge']      = pandas.to_numeric(dataset_1['Brooklyn Bridge
 dataset_1['Manhattan Bridge']     = pandas.to_numeric(dataset_1['Manhattan Bridge'].replace(',','', regex=True))
 dataset_1['Queensboro Bridge']    = pandas.to_numeric(dataset_1['Queensboro Bridge'].replace(',','', regex=True))
 dataset_1['Williamsburg Bridge']  = pandas.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
-# dataset_1['Williamsburg Bridge']  = pandas.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
+dataset_1['Total']  = pandas.to_numeric(dataset_1['Total'].replace(',','', regex=True))
 print(dataset_1.to_string()) #This line will print out your data
--- regression_attempt.py
+++ regression_attempt.py
@@ -2,12 +2,24 @@ from MiniProjectPath1 import dataset_1
 import numpy as np
 import datetime
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
-def input_X_matrix():
+def input_X_matrix(test_indeces):
     """returns a 5 column matrix such that data for each bridge is recorded in a single colmn vector"""
     Brooklyn, Manhattan = list(dataset_1['Brooklyn Bridge']), list(dataset_1['Manhattan Bridge'])
     Williamsburg, Queensboro = list(dataset_1['Williamsburg Bridge']), list(dataset_1['Queensboro Bridge'])
-    return np.array([Brooklyn, Manhattan, Williamsburg, Queensboro], 'F')
+    return np.matrix([Brooklyn, Manhattan, Williamsburg, Queensboro]).transpose()
+
+def output_y_matrix():
+    return np.matrix(list(dataset_1['Total'])).transpose()
+
+def split_data(X, y):
+    """shall return 4 lists of data: x_train, x_test; y_train, y_test"""
+    #using a 80-20 split: temporarily use None for random_state
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state= None)
+    return x_train, x_test, y_train, y_test
+
+def regression(X, y):
 
 
 def main():
